66a5855414219fd787f8722d174ce1aa65e4f314
ChebanTetyana <kapitoskay1302@gmail.com>
2024-06-25 13:33:03 +0300

    Create phpUnit project and create unit test

---
+++ src/Calculator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Cheba\PhpUnit;
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function multiplication($a, $b)
+    {
+        return $a * $b;
+    }
+}
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Cheba\tests;
+
+use Cheba\PhpUnit\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(2, 3);
+        $this->assertEquals(5, $result);
+    }
+    public function testMultiplication()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->multiplication(2, 3);
+        $this->assertEquals(6, $result);
+    }
+}
