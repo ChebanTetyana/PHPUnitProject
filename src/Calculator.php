db91c3d975aedf80ba3c6c944524d662d3835782
ChebanTetyana <kapitoskay1302@gmail.com>
2024-07-07 12:56:58 +0300

    Resolve aritmetic task and unit tests for them

--- src/Calculator.php
+++ src/Calculator.php
@@ -13,4 +13,42 @@ class Calculator
     {
         return $a * $b;
     }
+
+    public function isPrime($number)
+    {
+        if($number<=1) {
+            return false;
+        }
+
+        if($number<=3) {
+            return true;
+        }
+
+        if($number % 2==0|| $number % 3 == 0) {
+            return false;
+        }
+
+        for ($i = 5; $i * $i <= $number; $i += 6) {
+            if ($number % $i == 0 || $number % ($i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function gcd($a, $b)
+    {
+        while ($b != 0) {
+            $t = $b;
+            $b = $a % $b;
+            $a = $t;
+        }
+        return $a;
+    }
+
+    public function coprime($a, $b)
+    {
+        return $this->gcd($a, $b) == 1;
+    }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Cheba\tests;
+namespace Cheba\PhpUnit\tests;
 
 use Cheba\PhpUnit\Calculator;
 use PHPUnit\Framework\TestCase;
@@ -13,10 +13,32 @@ class CalculatorTest extends TestCase
         $result = $calculator->add(2, 3);
         $this->assertEquals(5, $result);
     }
+
     public function testMultiplication()
     {
         $calculator = new Calculator();
         $result = $calculator->multiplication(2, 3);
         $this->assertEquals(6, $result);
+    }
+
+    public function testIsPrime()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->isPrime(11);
+        $this->assertTrue($result);
+    }
+
+    public function testGcd()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->gcd(56, 98);
+        $this->assertEquals(14, $result);
+    }
+
+    public function testCoprime()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->coprime(35, 64);
+        $this->assertTrue($result);
     }
 }
