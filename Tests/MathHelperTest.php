4876dd99f92cc58c65207bea9c233333711ff27d
ChebanTetyana <kapitoskay1302@gmail.com>
2024-07-15 22:11:17 +0300

    Adding several options for unit tests

--- Tests/MathHelperTest.php
+++ Tests/MathHelperTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Cheba\PhpUnit\tests;
+namespace Cheba\PhpUnit\Tests;
 
 use Cheba\PhpUnit\MathHelper;
 use PHPUnit\Framework\TestCase;
@@ -10,21 +10,33 @@ class MathHelperTest extends TestCase
     public function testIsPrime()
     {
         $mathHelper = new MathHelper();
+
         $result = $mathHelper->isPrime(11);
         $this->assertTrue($result);
+
+        $result = $mathHelper->isPrime(12);
+        $this->assertFalse($result);
     }
 
     public function testGcd()
     {
         $mathHelper = new MathHelper();
+
         $result = $mathHelper->gcd(56, 98);
         $this->assertEquals(14, $result);
+
+        $result = $mathHelper->gcd(72, 27);
+        $this->assertEquals(9, $result);
     }
 
     public function testCoprime()
     {
         $mathHelper = new MathHelper();
+
         $result = $mathHelper->coprime(35, 64);
         $this->assertTrue($result);
+
+        $result = $mathHelper->coprime(14, 21);
+        $this->assertFalse($result);
     }
 }
