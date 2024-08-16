79e5c6f04a9394c15b6bf7687895c52179e2d3f2
ChebanTetyana <kapitoskay1302@gmail.com>
2024-08-16 16:19:38 +0300

    Testing for one figures

--- Tests/Services/ShapeCalculatorTest.php
+++ Tests/Services/ShapeCalculatorTest.php
@@ -2,10 +2,7 @@
 
 namespace Cheba\PhpUnit\Tests\Services;
 
-use Cheba\PhpUnit\Models\Circle;
 use Cheba\PhpUnit\Models\Rectangle;
-use Cheba\PhpUnit\Models\Square;
-use Cheba\PhpUnit\Models\Triangle;
 use Cheba\PhpUnit\Services\ShapeCalculator;
 use PHPUnit\Framework\TestCase;
 
@@ -16,16 +13,7 @@ class ShapeCalculatorTest extends TestCase
         $calculator = new ShapeCalculator();
 
         $rectangle = new Rectangle(4, 5);
-        $this->assertEquals(20, $calculator->calculate($rectangle));
-
-        $circle = new Circle(3);
-        $this->assertEqualsWithDelta(28.27, $calculator->calculate($circle), 0.01);
-
-        $square = new Square(4);
-        $this->assertEquals(16, $calculator->calculate($square));
-
-        $triangle = new Triangle(3, 4, 5);
-        $this->assertEquals(6, $calculator->calculate($triangle));
+        $this->assertEquals(20, $calculator->calculate($rectangle)['area']);
     }
 
     public function testCalculatePerimeter()
@@ -33,15 +21,6 @@ class ShapeCalculatorTest extends TestCase
         $calculator = new ShapeCalculator();
 
         $rectangle = new Rectangle(4, 5);
-        $this->assertEquals(18, $calculator->calculate($rectangle));
-
-        $circle = new Circle(3);
-        $this->assertEqualsWithDelta(18.85, $calculator->calculate($circle), 0.01);
-
-        $square = new Square(4);
-        $this->assertEquals(16, $calculator->calculate($square));
-
-        $triangle = new Triangle(3, 4, 5);
-        $this->assertEquals(12, $calculator->calculate($triangle));
+        $this->assertEquals(18, $calculator->calculate($rectangle)['perimeter']);
     }
 }
