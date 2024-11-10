7253ac7576220b9c6a5aa9a0b5f026b55b4194c8
ChebanTetyana <kapitoskay1302@gmail.com>
2024-11-10 18:33:07 +0200

    Add WholeSaleStrategy with discount logic

---
+++ Tests/StrategyAndFactoryDesignPattern/DiscountStrategyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cheba\PhpUnit\Tests\StrategyAndFactoryDesignPattern;
+
+use PHPUnit\Framework\TestCase;
+
+class DiscountStrategyTest extends TestCase
+{
+    public function testStandart()
+    {
+
+    }
+}
